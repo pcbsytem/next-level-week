cc076ebeb50032330afa0f10cbbf22fd1e14aba7
pcbsytem <pcbsytem@hotmail.com>
2020-06-02 22:41:47 -0300

    Changes in api structure

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem usuarios');
-
-  response.json([
-    'Patrick',
-    'Mario',
-    'Maria'
-  ]);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
